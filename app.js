4cf2b329ec684c8696136bc3b751207295ffa54b
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-03-17 11:45:09 -0500

    array into html

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ const letterW = document.getElementById('wspan');
 const letterX = document.getElementById('xspan');
 const letterY = document.getElementById('yspan');
 const letterZ = document.getElementById('zspan');
+
+// Win Count
+// Lose Count
+// Guess Left
+
+
+
 let words = [
     "apple", "kiwi", "orange", "1234567"
 ];
@@ -39,13 +46,35 @@ for (let i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
 
-function printArray() {
-    document.getElementById('answerArray');
-}
+var remainingLetters = word.length;
 
-printArray();
+document.getElementById('answerArray').innerHTML = answerArray.join(' ');
 
-// Game Loop
+// The game loop
+while (remainingLetters > 0) {
+    
+  // Show the player their progress
+  alert(answerArray.join(" "));
+  
+  // Get a guess from the player
+  var guess = prompt("Click a letter, or click Cancel to stop playing.");
+  if (guess === null) {
+  // Exit the game loop
+  break;
+  } else if (guess.length !== 1) {
+  alert("Please enter a single letter.");
+  } else {
+  // Update the game state with the guess
+  for (var j = 0; j < word.length; j++) {
+  if (word[j] === guess) {
+  answerArray[j] = guess;
+  remainingLetters--;
+  }
+  }
+  }
+
+  // The end of the game loop
+  }
 
 // Add a handler for the 'click' event by providing a callback function.
 // Whenever the element is clicked, letter* function executes
