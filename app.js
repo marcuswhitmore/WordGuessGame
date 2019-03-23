4ee35c9dc8e1da0173cf04537f7d29ee8a47593d
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-03-23 11:37:30 -0500

    adding new files

--- app.js
+++ app.js
@@ -1,99 +1,168 @@
+window.onload = function () {
 
-// Win Count
-// Lose Count
-// Guess Left
-
-
-
-let words = [
-    "aaaaa", "aaaaaaaa", "aaaa", "aaaaaaaaaaaa"
-];
-
-// Chooses a random word from the array and finds the length
-let word = words[Math.floor(Math.random() * words.length)];
-
-
-// Create Empty Array and fill it with underscores
-
-let answerArray = [];
-for (let i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
-
-var remainingLetters = word.length;
-
-
-
-// The game loop
-while (remainingLetters > 0) {
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+          'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+          't', 'u', 'v', 'w', 'x', 'y', 'z'];
     
-  // Show the player their progress
-  document.getElementById('answerArray').innerHTML = answerArray.join(' ');
+    var categories;         // Array of topics
+    var chosenCategory;     // Selected catagory
+    var getHint ;          // Word getHint
+    var word ;              // Selected word
+    var guess ;             // Geuss
+    var geusses = [ ];      // Stored geusses
+    var lives ;             // Lives
+    var counter ;           // Count correct geusses
+    var space;              // Number of spaces in word '-'
   
-  // Get a guess from the player
-  // var something.onClick will need to be added here
-  var guess = prompt("Click a letter, or click Cancel to stop playing.");
-  if (guess === null) {
-  // Exit the game loop
-  break;
-  } else if (guess.length !== 1) {
-  alert("Please enter a single letter.");
-  } else {
-  // Update the game state with the guess
-  for (var j = 0; j < word.length; j++) {
-  if (word[j] === guess) {
-  answerArray[j] = guess;
-  remainingLetters--;
-  }
-  }
-  }
-
-  // The end of the game loop
-  }
-
-// Add a handler for the 'click' event by providing a callback function.
-// Whenever the element is clicked, letter* function executes
-$('#aspan').on('click', function (e) {
-  // script for click to connect to letter A
-  console.log("a div a div")
-});
-/*
-//global variable so you can use it with all other letters
-var actualLetter;
-
-html: <button onclick="function letterA(<pass value>)" />
-js: function letterA (event){
-  actualLetter = event.key; //event.keyCode;
-
-}
-
-*/
-
-// removes need for constant at top and links selector to fucntion event
-$('#bspan').on('click', function (event) {
-    // on click do this
-    // compare click/letter selction to randomwordarray
-    // fill in spot if correct
-    // else say incorrect and increase guess by 1
-    //return to game loop?
-    console.log("b div click works");
-  });
-
-  $('#cspan').on('click', function (event) {
-    var number = 100000000 + Math.floor(Math.random() * 900000000)
-     // chooos random number
-
-     // make a loop that runs 9 times
-     // inside loop make a div with random number inserted
-      console.log(number);
+    // Get elements
+    var showLives = document.getElementById("mylives");
+    // var showCatagory = document.getElementById("scatagory");
+    // var getHint = document.getElementById("hint");
+    var showClue = document.getElementById("clue");
+  
+  
+  
+    // create alphabet ul
+    var buttons = function () {
+      myButtons = document.getElementById('buttons');
+      letters = document.createElement('ul');
+  
+      for (var i = 0; i < alphabet.length; i++) {
+        letters.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+        check();
+        myButtons.appendChild(letters);
+        letters.appendChild(list);
+      }
+    }
+      
     
-  });
+    // Select Catagory
+    var selectCat = function () {
+      if (chosenCategory === categories[0]) {
+        catagoryName.innerHTML = "The Chosen Category Is Super Hot Peppers";
+      } else if (chosenCategory === categories[1]) {
+        catagoryName.innerHTML = "The Chosen Category Is Popular Tomato Varities";
+      } else if (chosenCategory === categories[2]) {
+        catagoryName.innerHTML = "The Chosen Category Is Cities";
+      }
+    }
+  
+    // Create geusses ul
+     result = function () {
+      wordHolder = document.getElementById('hold');
+      correct = document.createElement('ul');
+  
+      for (var i = 0; i < word.length; i++) {
+        correct.setAttribute('id', 'my-word');
+        guess = document.createElement('li');
+        guess.setAttribute('class', 'guess');
+        if (word[i] === "-") {
+          guess.innerHTML = "-";
+          space = 1;
+        } else {
+          guess.innerHTML = "_";
+        }
+  
+        geusses.push(guess);
+        wordHolder.appendChild(correct);
+        correct.appendChild(guess);
+      }
+    }
+    
+    // Show lives
+     comments = function () {
+      showLives.innerHTML = "You have " + lives + " lives";
+      if (lives < 1) {
+        showLives.innerHTML = "Game Over";
+      }
+      for (var i = 0; i < geusses.length; i++) {
+        if (counter + space === geusses.length) {
+          showLives.innerHTML = "You Win!";
+        }
+      }
+    }
 
+  
 
-//   let log = document.getElementById('log');
-
-// document.onclick = inputChange;
-
-// function inputChange(e) {
-//   log.textContent = `Position: (${e.clientX}, ${e.clientY})`;
-// }
+  
+    // OnClick Function
+     check = function () {
+      list.onclick = function () {
+        var geuss = (this.innerHTML);
+        this.setAttribute("class", "active");
+        this.onclick = null;
+        for (var i = 0; i < word.length; i++) {
+          if (word[i] === geuss) {
+            geusses[i].innerHTML = geuss;
+            counter += 1;
+          } 
+        }
+        var j = (word.indexOf(geuss));
+        if (j === -1) {
+          lives -= 1;
+          comments();
+          animate();
+        } else {
+          comments();
+        }
+      }
+    }
+    
+      
+    // Play
+    play = function () {
+        categories = [
+            ["carolina-reaper", "ghost", "habanero", "seven-pot", "naga-viper", "trinidad-scorpion", "lemon-starburst"],
+            ["better-boy", "black-krim", "early-girl", "san-marzano", "brandy-wine"],
+            ["austin", "new-york", "san-francisco", "seatlle", "miami"]
+        ];
+  
+      chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+      word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+      word = word.replace(/\s/g, "-");
+      console.log(word);
+      buttons();
+  
+      geusses = [ ];
+      lives = 10;
+      counter = 0;
+      space = 0;
+      result();
+      comments();
+      selectCat();
+    //   canvas();
+    }
+  
+    play();
+    
+    // Hint
+  
+      hint.onclick = function() {
+  
+        hints = [
+            ["2.2 Million Scoville", "1 Million Scoville", "350k Scoville", "1.8 Million Scoville", "1.4 Million Scoville", "2 Million Scoville", "Khang Star Variety"],
+            ["Guinness record holder for amount of fruit produced from a single plant", "Heirloom Tomato from Russia", "Early Ripening Fruit", "Most Popular for Sauces", "Potato-leaved foliage which bears large pink beefsteak-shaped fruit"],
+            ["Capitol of Texas", "Hungry for Apples?", "Trolleys and Hills", "Emerald Triangle", "Only US City founded by a woman"]
+        ];
+  
+      var catagoryIndex = categories.indexOf(chosenCategory);
+      var hintIndex = chosenCategory.indexOf(word);
+      showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
+    };
+  
+     // Reset
+  
+    document.getElementById('reset').onclick = function() {
+      correct.parentNode.removeChild(correct);
+      letters.parentNode.removeChild(letters);
+      showClue.innerHTML = "";
+      context.clearRect(0, 0, 400, 400);
+      play();
+    }
+  }
+  
+  
+  
